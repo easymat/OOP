34ceee7a97a82b2b8dc11b0951983565424878cd
Natalya Khusnutdinova <easymatika@yandex.ru>
2025-02-06 09:34:04 +0300

    Add child class <<MobilePhone>>

--- Phones.py
+++ Phones.py
@@ -15,16 +15,29 @@ class Phone:
 class MobilePhone(Phone):
 
     line_type = 'беспроводной'
+    battery_type = 'Li-ion'
+
+    def __init__(self, dial_type_value, network_type):
+        self.network_type = network_type
+        super().__init__(dial_type_value)
 
     def ring(self):
         print('Дзынь-дзынь!')
 
+    def start_game(self):
+        print('Игра запущена!')
+
 
 rotary_phone = Phone('дисковый')
-mobile_phone = MobilePhone('сенсорный')
+mobile_phone = MobilePhone('сенсорный', 'LTE')
 
 print(rotary_phone.line_type)
 rotary_phone.ring()
 
 print(mobile_phone.line_type)
 mobile_phone.ring()
+
+print(mobile_phone.battery_type)
+print(mobile_phone.network_type)
+mobile_phone.start_game()
+print(rotary_phone.__str__())
