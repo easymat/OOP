4356a39d4815e782c03cfe3a96f35bd798330167
Natalya Khusnutdinova <easymatika@yandex.ru>
2025-02-06 20:25:30 +0300

    Add get_remainig_vacation_day in OfficeEmployee

--- Employee.py
+++ Employee.py
@@ -1,14 +1,32 @@
 class Employee:
     vacation_days = 28
 
-    def __init__(self, first_name, second_name, gender):
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            gender: str
+    ):
         self.first_name = first_name
-        self.second_name = second_name
+        self.last_name = last_name
         self.gender = gender
+        self._employee_id = self.__generate_employee_id(first_name, last_name, gender)
         self.remaining_vacation_days = self.vacation_days
 
-    def consume_vacation(self, days):
+    def __generate_employee_id(
+            self,
+            first_name: str,
+            last_name: str,
+            gender: str
+    ):
+        employee_id = hash(first_name + last_name + gender)
+        return employee_id
+
+    def consume_vacation(self, days: int):
         self.remaining_vacation_days -= days
+
+    def get_remaining_vacation_days(self) -> int:
+        return self.remaining_vacation_days
 
     def get_vacation_details(self):
         return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
@@ -16,8 +34,8 @@ class Employee:
 
 class FullTimeEmployee(Employee):
 
-    def __init__(self, first_name, second_name, gender):
-        super().__init__(first_name, second_name, gender)
+    def __init__(self, first_name, last_name, gender):
+        super().__init__(first_name, last_name, gender)
 
     def get_unpaid_vacation(self, first_vacation_day, number_of_days):
         self.first_day = first_vacation_day
@@ -34,10 +52,35 @@ class PartTimeEmployee(Employee):
         super().__init__(first_name, second_name, gender)
 
 
-# Расширьте класс Employee, создав классы FullTimeEmployee и PartTimeEmployee.
+class OfficeEmployee(Employee):
+
+    def __init__(self, first_name, last_name, gender, worker_class: int, salary: int):
+        self.worker_class = worker_class
+        self.__salary = salary
+        super().__init__(first_name, last_name, gender)
+
+    def get_remaining_vacation_days(self) -> int:
+        return self.remaining_vacation_days + self.worker_class * 2
+
+    def get_vacation_payment(self, days_of_vacation):
+        self.how_much_to_pay = self.__salary * days_of_vacation // 60
+        return self.how_much_to_pay
+
 
 # Пример использования:
-full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
-print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
-part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
-print(part_time_employee.get_vacation_details())
+office_employee = OfficeEmployee(first_name='Иван', last_name='Иванов', gender='м', worker_class=2, salary=45000)
+
+vacation_days = 10
+
+office_employee.consume_vacation(vacation_days)
+
+remaining_days = office_employee.get_remaining_vacation_days()
+print(f'У сотрудника осталось {remaining_days} дн. отпуска.')
+
+vacation_payment = office_employee.get_vacation_payment(vacation_days)
+print(f'За {vacation_days} дн. отпуска сотрудник получит {vacation_payment} руб.')
+
+#full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+#print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+#part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+#print(part_time_employee.get_vacation_details())
