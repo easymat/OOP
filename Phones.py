248949680f94af02d03d744110ca2cca88b92e62
Natalya Khusnutdinova <easymatika@yandex.ru>
2025-02-06 08:46:52 +0300

    Add first commit

--- Phones.py
+++ Phones.py
@@ -1,17 +1,16 @@
 class Phone:
-    
-    line_type = 'проводной'
 
+    line_type = 'проводной'
 
     def __init__(self, dial_type_value):
         dial_type = dial_type_value
-
 
     def ring(self):
         print('Дзззззззыыыыыыыыынь!')
 
     def call(self, phone_number):
         print(f'Звоню по номеру {phone_number}! Тип связи - {self.line_type}')
+
 
 class MobilePhone(Phone):
 
@@ -25,7 +24,7 @@ rotary_phone = Phone('дисковый')
 mobile_phone = MobilePhone('сенсорный')
 
 print(rotary_phone.line_type)
-rotary_phone.ring
+rotary_phone.ring()
 
 print(mobile_phone.line_type)
-mobile_phone.ring
+mobile_phone.ring()
